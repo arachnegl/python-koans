3b8552606d851b49e512e692fa2d9877d7155eb0
greg loyse <gloyse@google.com>
2014-12-07 19:03:14 +0000

    de-unittested

--- koans/about_strings.py
+++ koans/about_strings.py
@@ -18,19 +18,19 @@ class AboutStrings(unittest.TestCase):
 
         string = "Hello, world."
 
-        self.assertEqual(__, isinstance(string, str))
+        assert __, isinstance(string, str)
 
     def test_single_quoted_strings_are_also_strings(self):
 
         string = 'Goodbye, world.'
 
-        self.assertEqual(__, isinstance(string, str))
+        assert __, isinstance(string, str)
 
     def test_triple_quote_strings_are_also_strings(self):
 
         string = """Howdy, world!"""
 
-        self.assertEqual(__, isinstance(string, str))
+        assert __, isinstance(string, str)
 
 
     # How to create strings that contain quotes
@@ -39,27 +39,27 @@ class AboutStrings(unittest.TestCase):
 
         string = 'He said, "Go Away."'
 
-        self.assertEqual(__, string)
+        assert __, string
 
     def test_use_double_quotes_to_create_strings_with_single_quotes(self):
 
         string = "Don't"
 
-        self.assertEqual(__, string)
+        assert __, string
 
     def test_use_backslash_for_escaping_quotes_in_strings(self):
 
         a = "He said, \"Don't\""
         b = 'He said, "Don\'t"'
 
-        self.assertEqual(__, (a == b))
+        assert __, (a == b)
 
     def test_triple_quoted_strings_need_less_escaping(self):
 
         a = "Hello \"world\"."
         b = """Hello "world"."""
 
-        self.assertEqual(__, (a == b))
+        assert __, (a == b)
 
 
     # Functions & String methods
@@ -68,13 +68,13 @@ class AboutStrings(unittest.TestCase):
 
         string = "Hello, " + "world"
 
-        self.assertEqual(__, string)
+        assert __, string
 
     def test_adjacent_strings_are_concatenated_automatically(self):
 
         string = "Hello" ", " "world"
 
-        self.assertEqual(__, string)
+        assert __, string
 
     def test_plus_will_not_modify_original_strings(self):
 
@@ -82,8 +82,8 @@ class AboutStrings(unittest.TestCase):
         there = "world"
         string = hi + there
 
-        self.assertEqual(__, hi)
-        self.assertEqual(__, there)
+        assert __, hi
+        assert __, there
 
     def test_plus_equals_will_append_to_end_of_string(self):
 
@@ -91,10 +91,10 @@ class AboutStrings(unittest.TestCase):
         there = "world"
         hi += there
 
-        self.assertEqual(__, hi)
+        assert __, hi
 
     def test_len_returns_length_of_string(self):
 
         hi = "hi"
 
-        self.assertEqual(__, len(hi))
+        assert __, len(hi)
