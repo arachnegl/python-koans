27fa208ac9d64cb2ca79e2b659330edd7d7e4456
greg loyse <gloyse@google.com>
2014-12-10 21:34:43 +0000

    de-unittested lists

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from runner.koan import __
+
+__ = False
 
 
 class AboutLists(unittest.TestCase):
@@ -11,96 +12,96 @@ class AboutLists(unittest.TestCase):
 
         empty_list = list()
 
-        self.assertEqual(list, type(empty_list))
-        self.assertEqual(__, len(empty_list))
+        assert list == type(empty_list)
+        assert __ == len(empty_list)
 
     def test_list_literals(self):
 
         nums = list()
-        self.assertEqual([], nums)
+        assert [] == nums
 
         nums[0:] = [1]
-        self.assertEqual([1], nums)
+        assert [1] ==  nums
 
         nums[1:] = [2]
-        self.assertListEqual([1, __], nums)
+        assert [1, __] == nums
 
         nums.append(333)
-        self.assertListEqual([1, 2, __], nums)
+        assert [1, 2, __] == nums
 
     def test_accessing_list_elements(self):
 
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[0])
-        self.assertEqual(__, noms[3])
-        self.assertEqual(__, noms[-1])
-        self.assertEqual(__, noms[-3])
+        assert __ == noms[0]
+        assert __ == noms[3]
+        assert __ == noms[-1]
+        assert __ == noms[-3]
 
     def test_slicing_lists(self):
 
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[0:1])
-        self.assertEqual(__, noms[0:2])
-        self.assertEqual(__, noms[2:2])
-        self.assertEqual(__, noms[2:20])
-        self.assertEqual(__, noms[4:0])
-        self.assertEqual(__, noms[4:100])
-        self.assertEqual(__, noms[5:0])
+        assert __ == noms[0:1]
+        assert __ == noms[0:2]
+        assert __ == noms[2:2]
+        assert __ == noms[2:20]
+        assert __ == noms[4:0]
+        assert __ == noms[4:100]
+        assert __ == noms[5:0]
 
     def test_slicing_to_the_edge(self):
 
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[2:])
-        self.assertEqual(__, noms[:2])
+        assert __ == noms[2:]
+        assert __ == noms[:2]
 
     def test_lists_and_ranges(self):
 
-        self.assertEqual(range, type(range(5)))
-        self.assertNotEqual([1, 2, 3, 4, 5], range(1, 6))
-        self.assertEqual(__, list(range(5)))
-        self.assertEqual(__, list(range(5, 9)))
+        assert range == type(range(5))
+        assert not [1, 2, 3, 4, 5] == range(1, 6)
+        assert __ == list(range(5))
+        assert __ == list(range(5, 9))
 
     def test_ranges_with_steps(self):
 
-        self.assertEqual(__, list(range(0, 8, 2)))
-        self.assertEqual(__, list(range(1, 8, 3)))
-        self.assertEqual(__, list(range(5, -7, -4)))
-        self.assertEqual(__, list(range(5, -8, -4)))
+        assert __ == list(range(0, 8, 2))
+        assert __ == list(range(1, 8, 3))
+        assert __ == list(range(5, -7, -4))
+        assert __ == list(range(5, -8, -4))
 
     def test_insertions(self):
 
         knight = ['you', 'shall', 'pass']
         knight.insert(2, 'not')
-        self.assertEqual(__, knight)
+        assert __ == knight
 
         knight.insert(0, 'Arthur')
-        self.assertEqual(__, knight)
+        assert __ == knight
 
     def test_popping_lists(self):
 
         stack = [10, 20, 30, 40]
         stack.append('last')
 
-        self.assertEqual(__, stack)
+        assert __ == stack
 
         popped_value = stack.pop()
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, stack)
+        assert __ == popped_value
+        assert __ == stack
 
         popped_value = stack.pop(1)
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, stack)
+        assert __ == popped_value
+        assert __ == stack
 
     def test_making_queues(self):
 
         queue = [1, 2]
         queue.append('last')
 
-        self.assertEqual(__, queue)
+        assert __ == queue
 
         popped_value = queue.pop(0)
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, queue)
+        assert __ == popped_value
+        assert __ == queue
