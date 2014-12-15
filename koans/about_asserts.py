d48f9c946e3c7468801e69e222ffd06a0ca1228d
greg loyse <gloyse@google.com>
2014-12-15 09:07:12 +0000

    removed solutions

--- koans/about_asserts.py
+++ koans/about_asserts.py
@@ -10,15 +10,15 @@ class AboutAsserts(unittest.TestCase):
 
     def test_assert_truth(self):
 
-        assert True # replace with True
+        assert False # replace with True
 
     def test_equality(self):
 
-        assert 2 == 1 + 1
+        assert __ == 1 + 1
 
     def test_equality_with_assignment(self):
 
-        expected_value = 2
+        expected_value = __
         actual_value = 1 + 1
 
         assert expected_value == actual_value
